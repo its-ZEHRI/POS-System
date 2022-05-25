bedf8398f92d72f644cae4abec8d0c43ad2235cb
its-ijaz <ijazkhan7706@gmail.com>
2022-05-25 12:26:34 +0500

    added landing page and design change of login page

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,39 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\InventoryController;
 use App\Http\Controllers\PurchaseController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TempProductController;
 
 Auth::routes();
 
 Route::group(['middleware'=>['auth']],function(){
-
-    Route::get('/home', [HomeController::class, 'index']);
-    Route::get('purchase',[PurchaseController::class,'index']);
-    Route::get('/refreshPurchase',[TempProductController::class,'refresh']);
-    Route::get('inventory',[InventoryController::class, 'index']);
+    Route::get('/dashboard',        [DashboardController::class,   'index'  ]);
+    Route::get('/purchase',         [PurchaseController::class,    'index'  ]);
+    Route::get('/refreshPurchase',  [TempProductController::class, 'refresh']);
+    Route::get('/inventory',        [InventoryController::class,   'index'  ]);
 
     Route::group(['prefix'=>'/purchase'],function(){
-        Route::post('/tempCreateData',[TempProductController::class,'tempCreate']);
-        Route::post('/tempUpdateData', [TempProductController::class, 'tempUpdate']);
-        Route::get('/destroy/{id}', [TempProductController::class, 'destroy']);
+        Route::post('/tempCreateData',  [TempProductController::class, 'tempCreate' ]);
+        Route::post('/tempUpdateData',  [TempProductController::class, 'tempUpdate' ]);
+        Route::get ('/destroy/{id}',    [TempProductController::class, 'destroy'    ]);
     });
 
     Route::group(['prefix'=> '/setting'],function(){
-        Route::post('/createCategory',[CategoryController::class,'createCategory']);
+        Route::post('/createCategory',  [CategoryController::class, 'createCategory']);
     });
 
     Route::get('setting', [CategoryController::class, 'index']);
 
-    Route::get('dashboard',function(){
-        return view('app.dashboard');
-    });
+
     Route::get('sale',function(){
         return view('app.sale');
     });
 
-
-});
+});// END OF AUTH MIDDLEWARE
 
 Route::get('/', function () {
     return view('home');
